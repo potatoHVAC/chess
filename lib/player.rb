5048756fb2299e4864b93ef700c284306a360b8d
daniel speer <d@niel.nothing>
2018-03-17 19:37:50 -0700

    change front_row and back_row to army array

--- lib/player.rb
+++ lib/player.rb
@@ -1,38 +1,38 @@
 class Player
 
   attr_reader :color, :name, :king
-  attr_accessor :front_row, :back_row, :dead
+  attr_accessor :army, :dead
   def initialize(color, name)
     @name = name
     @color = color
     @dead = []
 
-    generate_lineup
+    @army = generate_lineup
   end
 
   def generate_lineup
     @king = King.new(color)
-    @front_row = Array.new
-    8.times { @front_row << Pawn.new(color) }
-    @back_row = [Rook.new(color),
+    pawn_row = Array.new
+    8.times { pawn_row << Pawn.new(color) }
+    royal_row = [Rook.new(color),
                  Knight.new(color),
                  Bishop.new(color),
                  Queen.new(color),
                  king,
                  Bishop.new(color),
                  Knight.new(color),
-                 Rook.new(color)
-                ]
+                 Rook.new(color)]
+    [pawn_row, royal_row]
   end
 
   def insert_pieces(board)
     case self.color
     when :black
-      front_row.each_with_index { |piece, i| board.insert([i, 6], piece) }
-      back_row.each_with_index { |piece, i| board.insert([i, 7], piece) }
+      army[0].each_with_index { |piece, i| board.insert([i, 6], piece) }
+      army[1].each_with_index { |piece, i| board.insert([i, 7], piece) }
     else
-      front_row.each_with_index { |piece, i| board.insert([i, 1], piece) }
-      back_row.each_with_index { |piece, i| board.insert([i, 0], piece) }
+      army[0].each_with_index { |piece, i| board.insert([i, 1], piece) }
+      army[1].each_with_index { |piece, i| board.insert([i, 0], piece) }
     end
     
     board    
@@ -40,10 +40,10 @@ class Player
   
   def cap(piece)
     if piece.type == :pawn
-      @front_row.reject! {|p| p == piece }
+      @army[0].reject! {|p| p == piece }
     else
       piece.has_moved
-      @back_row.reject! {|p| p == piece }
+      @army[1].reject! {|p| p == piece }
       @dead << piece
     end
   end
@@ -58,13 +58,12 @@ class Player
 
   def revive(piece)
     @dead.reject! {|p| p == piece }
-    @back_row << piece
+    @army[1] << piece
     piece
   end
   
   def to_s
-    [@front_row.map(&:to_s).join(" "), @back_row.map(&:to_s).join(" ")].join("\n")
-  end
-      
+    [army.map(&:to_s).join(" ")].join("\n")
+  end  
 end
 
